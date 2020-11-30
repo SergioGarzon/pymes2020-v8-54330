2a27b13979737ba7d60f473ad111552ae21bce91
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 19:12:24 -0300

    1 file modified 2

--- src/app/components/categorias/categorias.component.ts
+++ src/app/components/categorias/categorias.component.ts
@@ -7,13 +7,12 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ModalDialogService } from "../../services/modal-dialog.service";
 
 @Component({
-  selector: 'app-categorias',
-  templateUrl: './categorias.component.html',
-  styleUrls: ['./categorias.component.css']
+  selector: "app-categorias",
+  templateUrl: "./categorias.component.html",
+  styleUrls: ["./categorias.component.css"]
 })
 export class CategoriasComponent implements OnInit {
-
-  Titulo = "Articulos";
+  Titulo = "Categorias";
   TituloAccionABMC = {
     A: "(Agregar)",
     B: "(Eliminar)",
@@ -21,25 +20,17 @@ export class CategoriasComponent implements OnInit {
     C: "(Consultar)",
     L: "(Listado)"
   };
-  AccionABMC = "L"; // inicialmente inicia en el listado de articulos (buscar con parametros)
+  AccionABMC = "L"; // inicialmente inicia en el listado de paises (buscar con parametros)
   Mensajes = {
     SD: " No se encontraron registros...",
     RD: " Revisar los datos ingresados..."
   };
 
-  Lista: Articulo[] = [];
+  Lista: Categoria[] = [];
   RegistrosTotal: number;
-  Familias: ArticuloFamilia[] = [];
   SinBusquedasRealizadas = true;
 
   Pagina = 1; // inicia pagina 1
-
-  // opciones del combo activo
-  OpcionesActivo = [
-    { Id: null, Nombre: "" },
-    { Id: true, Nombre: "SI" },
-    { Id: false, Nombre: "NO" }
-  ];
 
   FormFiltro: FormGroup;
   FormReg: FormGroup;
@@ -47,32 +38,18 @@ export class CategoriasComponent implements OnInit {
 
   constructor(
     public formBuilder: FormBuilder,
-    //private articulosService: MockArticulosService,
-    //private articulosFamiliasService: MockArticulosFamiliasService,
-    private articulosService: ArticulosService,
-    private articulosFamiliasService: ArticulosFamiliasService,
+    private categoriasService: CategoriasService,
     private modalDialogService: ModalDialogService
   ) {}
 
   ngOnInit() {
-    this.FormFiltro = this.formBuilder.group({
-      Nombre: [""],
-      Activo: [null]
-    });
     this.FormReg = this.formBuilder.group({
-      IdArticulo: [0],
+      IdPais: [0],
       Nombre: [
         "",
-        [Validators.required, Validators.minLength(4), Validators.maxLength(55)]
+        [Validators.required, Validators.minLength(4), Validators.maxLength(30)]
       ],
-      Precio: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
-      Stock: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
-      CodigoDeBarra: [
-        "",
-        [Validators.required, Validators.pattern("[0-9]{13}")]
-      ],
-      IdArticuloFamilia: ["", [Validators.required]],
-      FechaAlta: [
+      FechaCenso: [
         "",
         [
           Validators.required,
@@ -81,77 +58,50 @@ export class CategoriasComponent implements OnInit {
           )
         ]
       ],
-      Activo: [true]
-    });
-
-    this.GetFamiliasArticulos();
-  }
-
-  GetFamiliasArticulos() {
-    this.articulosFamiliasService.get().subscribe((res: ArticuloFamilia[]) => {
-      this.Familias = res;
+      Poblacion: [
+        null,
+        [Validators.required, Validators.pattern("[0-9]{1,10}")]
+      ]
     });
   }
 
   Agregar() {
     this.AccionABMC = "A";
-    this.FormReg.reset({ Activo: true });
+    //this.FormReg.reset({ Activo: true });
     this.submitted = false;
-    //this.FormReg.markAsPristine();
     this.FormReg.markAsUntouched();
   }
 
-  // Buscar segun los filtros, establecidos en FormReg
   Buscar() {
     this.SinBusquedasRealizadas = false;
-    this.articulosService
-      .get(
-        this.FormFiltro.value.Nombre,
-        this.FormFiltro.value.Activo,
-        this.Pagina
-      )
-      .subscribe((res: any) => {
-        this.Lista = res.Lista;
-        this.RegistrosTotal = res.RegistrosTotal;
-      });
+    this.categoriasService.get().subscribe((res: any) => {
+      this.Lista = res;
+      this.RegistrosTotal = res.RegistrosTotal;
+    });
   }
 
-  // Obtengo un registro especifico según el Id
+  /*
   BuscarPorId(Dto, AccionABMC) {
     window.scroll(0, 0); // ir al incio del scroll
 
-    this.articulosService.getById(Dto.IdArticulo).subscribe((res: any) => {
+    this.categoriasService.getById(Dto.IdCategoria).subscribe((res: any) => {
       this.FormReg.patchValue(res);
 
       //formatear fecha de  ISO 8061 a string dd/MM/yyyy
-      var arrFecha = res.FechaAlta.substr(0, 10).split("-");
-      this.FormReg.controls.FechaAlta.patchValue(
+      var arrFecha = res.FechaCenso.substr(0, 10).split("-");
+      this.FormReg.controls.FechaCenso.patchValue(
         arrFecha[2] + "/" + arrFecha[1] + "/" + arrFecha[0]
       );
 
       this.AccionABMC = AccionABMC;
     });
-  }
+  }*/
 
+  /*
   Consultar(Dto) {
     this.BuscarPorId(Dto, "C");
-  }
+  }*/
 
-  // comienza la modificacion, luego la confirma con el metodo Grabar
-  Modificar(Dto) {
-    if (!Dto.Activo) {
-      this.modalDialogService.Alert(
-        "No puede modificarse un registro Inactivo."
-      );
-      return;
-    }
-    this.submitted = false;
-    this.FormReg.markAsPristine();
-    this.FormReg.markAsUntouched();
-    this.BuscarPorId(Dto, "M");
-  }
-
-  // grabar tanto altas como modificaciones
   Grabar() {
     this.submitted = true;
     // verificar que los validadores esten OK
@@ -163,67 +113,26 @@ export class CategoriasComponent implements OnInit {
     const itemCopy = { ...this.FormReg.value };
 
     //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
-    var arrFecha = itemCopy.FechaAlta.substr(0, 10).split("/");
+    var arrFecha = itemCopy.FechaAct.substr(0, 10).split("/");
     if (arrFecha.length == 3)
-      itemCopy.FechaAlta = new Date(
+      itemCopy.FechaAct = new Date(
         arrFecha[2],
         arrFecha[1] - 1,
         arrFecha[0]
       ).toISOString();
 
     // agregar post
-    if (itemCopy.IdArticulo == 0 || itemCopy.IdArticulo == null) {
-      itemCopy.IdArticulo = 0;
-      this.articulosService.post(itemCopy).subscribe((res: any) => {
+    if (itemCopy.IdCategoria == 0 || itemCopy.IdCategoria == null) {
+      itemCopy.IdCategoria = 0;
+      this.categoriasService.post(itemCopy).subscribe((res: any) => {
         this.Volver();
         this.modalDialogService.Alert("Registro agregado correctamente.");
         this.Buscar();
       });
-    } else {
-      // modificar put
-      this.articulosService
-        .put(itemCopy.IdArticulo, itemCopy)
-        .subscribe((res: any) => {
-          this.Volver();
-          this.modalDialogService.Alert("Registro modificado correctamente.");
-          this.Buscar();
-        });
     }
   }
 
-  // representa la baja logica
-  ActivarDesactivar(Dto) {
-    this.modalDialogService.Confirm(
-      "Esta seguro de " +
-        (Dto.Activo ? "desactivar" : "activar") +
-        " este registro?",
-      undefined,
-      undefined,
-      undefined,
-      () =>
-        this.articulosService
-          .delete(Dto.IdArticulo)
-          .subscribe((res: any) => this.Buscar()),
-      null
-    );
-  }
-
-  // Volver desde Agregar/Modificar
   Volver() {
     this.AccionABMC = "L";
   }
-
-  ImprimirListado() {
-    this.modalDialogService.Alert("Sin desarrollar...");
-  }
-
-  GetArticuloFamiliaNombre(Id) {
-    var ArticuloFamilia = this.Familias.filter(
-      x => x.IdArticuloFamilia === Id
-    )[0];
-    if (ArticuloFamilia) return ArticuloFamilia.Nombre;
-    else return "";
-  }
-
-
 }
