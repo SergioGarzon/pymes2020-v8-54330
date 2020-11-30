911a99652bc5dddc648f76e16b3bbf97d97f3504
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 18:57:42 -0300

    1 file se guardan cambios

--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Pais } from "../models/pais";
+
+@Injectable({
+  providedIn: "root"
+})
 
 @Injectable()
 export class CategoriasService {
